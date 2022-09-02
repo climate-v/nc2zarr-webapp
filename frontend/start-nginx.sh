e9155602fd7a9a290a7dadb56e907a228e27ab60
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-09-02 11:04:18 +0200

    [TEST] Use ENTRYPOINT instead of CMD.

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+set - Ee;
 pwd;
 cp /usr/share/nginx/main.*.js .;
 for f in main.*.js; do envsubst '$NC2ZARR_BACKEND_URL,$NC2ZARR_CONTENT_URL' < $f | sponge $f; done
-nginx -g daemon off;
+exec "$@"
+nginx -g "daemon off;"
