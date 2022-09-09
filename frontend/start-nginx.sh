e8d9acaf4ae74dde9e6a991f35c71d30d3d2ddc7
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-09-09 09:36:13 +0200

    [TEST] Use absolute paths for runtime configuration.

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set - Ee;
+cd /usr/share/nginx/html;
 pwd;
 cp /usr/share/nginx/main.*.js .;
 for f in main.*.js; do envsubst '$NC2ZARR_BACKEND_URL,$NC2ZARR_CONTENT_URL' < $f | sponge $f; done
