951ee2faf24443a1b0a5aaeece91cb831a815f0c
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-09-09 09:45:21 +0200

    [TEST] Encountered error: '/usr/share/nginx/html/start-nginx.sh: 7: exec: Ee: not found'.

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -4,5 +4,4 @@ cd /usr/share/nginx/html;
 pwd;
 cp /usr/share/nginx/main.*.js .;
 for f in main.*.js; do envsubst '$NC2ZARR_BACKEND_URL,$NC2ZARR_CONTENT_URL' < $f | sponge $f; done
-exec "$@"
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
